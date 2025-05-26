ba056596b88a1d61879d150cc8cf12562303d83e
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2025-05-26 22:05:01 +0200

    temporary fix for import errors

--- core/Initialize-Module.ps1
+++ core/Initialize-Module.ps1
@@ -1,16 +1,19 @@
-# import necessary functions
-.$PSScriptRoot\..\helpers\Install-Category.ps1
-.$PSScriptRoot\..\helpers\Install-WithWinget.ps1
-.$PSScriptRoot\..\helpers\Install-PSModule.ps1
-.$PSScriptRoot\..\helpers\Test-Installation.ps1
-.$PSScriptRoot\..\helpers\Update-Environment.ps1
+# # import necessary functions
+# .$PSScriptRoot\..\helpers\Install-Category.ps1
+# .$PSScriptRoot\..\helpers\Install-WithWinget.ps1
+# .$PSScriptRoot\..\helpers\Install-PSModule.ps1
+# .$PSScriptRoot\..\helpers\Test-Installation.ps1
+# .$PSScriptRoot\..\helpers\Update-Environment.ps1
 
-# Execute when the module is imported.
-Write-Host "Initializing module $ModuleName..." -ForegroundColor Cyan
-try {
-    Install-Category -Category 'Initialize' -ConfigLocation "$ModulePath\config\apps.json" -ErrorAction Stop
+# $ModuleName = Split-Path (Split-Path $PSScriptRoot -Parent) -Leaf
+# $ModulePath = Split-Path -Path $PSScriptRoot -Parent  
 
-    Write-Host "Module $ModuleName imported successfully." -ForegroundColor Green
-} catch {
-    Write-Error "Failed to import module $ModuleName : $_"
-}
+# # Execute when the module is imported.
+# Write-Host "Initializing module $ModuleName..." -ForegroundColor Cyan
+# try {
+#     Install-Category -Category 'Initialize' -ConfigLocation "$ModulePath\config\apps.json" -ErrorAction Stop
+
+#     Write-Host "Module $ModuleName imported successfully." -ForegroundColor Green
+# } catch {
+#     Write-Error "Failed to import module $ModuleName : $_"
+# }
